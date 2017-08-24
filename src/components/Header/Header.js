de9cc2882c577079a5d3e94e62fb81a8bd64df63
piotros <piotros@wp.pl>
2017-08-24 10:55:21 +0200

    Change Header component from function to class

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,26 +4,30 @@ import PropTypes from 'prop-types';
 import './Header.css';
 import icon from './react.png';
 
-const Header = function ({children, ComponentToUse, componentToUse}) {
-  if (componentToUse) {
+class Header extends React.Component {
+  render() {
+    const {children, ComponentToUse, componentToUse} = this.props;
+
+    if (componentToUse) {
+      return (
+        <h1>{React.createElement(componentToUse, {text: children})}</h1>
+      )
+    }
+
+    if (ComponentToUse) {
+      return (
+        <h1><ComponentToUse text={children}/></h1>
+      )
+    }
+
     return (
-      <h1>{React.createElement(componentToUse, {text: children})}</h1>
+      <h1 className="header">
+        <img src={icon} alt="React logo"/>
+        {children}
+      </h1>
     )
   }
-
-  if (ComponentToUse) {
-    return (
-      <h1><ComponentToUse text={children}/></h1>
-    )
-  }
-
-  return (
-    <h1 className="header">
-      <img src={icon} alt="React logo"/>
-      {children}
-    </h1>
-  )
-};
+}
 
 Header.defaultProps = {
   children: '!'
