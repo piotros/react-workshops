25e6e50606c5ca14fa36be940bb359038dda0530
piotros <piotros@wp.pl>
2017-08-24 16:16:15 +0200

    Remove unused values

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,6 @@ import React from 'react';
 import ClassTest from './ClassTest'
 
 export default function App(props) {
-  const values = {
-    input: 'dupa',
-    checkbox: true,
-    select: 'three'
-  }
-
-  const onSubmit = values => {
-    console.log(values);
-  }
-
   return (
     <div>
       <ClassTest className="text" classes={['test1', 'test2']} color="red"/>
