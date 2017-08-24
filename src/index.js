e83416f0857c2224c8d8bb18a107aea63936317d
piotros <piotros@wp.pl>
2017-08-24 09:06:03 +0200

    Add Header component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import {render} from 'react-dom';
+import Header from './components/Header';
 
-setInterval(() => {
-  render(<h1>Date: {Date()}</h1>, document.getElementById('root'))
-}, 1000);
-
-setInterval(() => {
-  render(<h1>Date: {Date()}</h1>, document.getElementById('root2'))
-}, 2000);
-
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+render(<Header />, document.getElementById('root'));
