2171c00ab26c21bd75b79f404f7571d03d3adf1a
piotros <piotros@wp.pl>
2017-08-24 14:11:15 +0200

    Pass values and callback to uncontrolled form through props

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import FormUncontrolled from "./FormUncontrolled/FormUncontrolled"
+import FormUncontrolled from "./FormUncontrolled"
 
 export default function App(props) {
+  const values = {
+    input: 'dupa',
+    checkbox: true,
+    select: 'three'
+  }
+
+  const onSubmit = values => {
+    console.log(values);
+  }
+
   return (
     <div>
-      <FormUncontrolled/>
+      <FormUncontrolled values={values} onSubmit={onSubmit}/>
     </div>
   )
 }
--- src/components/FormUncontrolled/FormUncontrolled.js
+++ src/components/FormUncontrolled/FormUncontrolled.js
@@ -4,30 +4,36 @@ class FormUncontrolled extends Component {
   handleSubmit = event => {
     event.preventDefault();
 
-    console.log(this.input.value);
-    console.log(this.checkbox.checked);
-    console.log(this.select.options[this.select.selectedIndex].value);
+    const values = {
+      input: this.input.value,
+      checkbox: this.checkbox.checked,
+      select: this.select.options[this.select.selectedIndex].value
+    }
+
+    this.props.onSubmit(values);
   }
 
   render() {
+    const {input, checkbox, select} = this.props.values;
+
     return (
       <form onSubmit={this.handleSubmit}>
         <input type="text"
-               defaultValue="test"
+               defaultValue={input}
                ref={input => this.input = input}
         />
 
         <br/>
 
         <input type="checkbox"
-               defaultValue={false}
+               defaultChecked={checkbox}
                ref={checkbox => this.checkbox = checkbox}
         />
 
         <br/>
 
         <select
-          defaultValue="one"
+          defaultValue={select}
           ref={select => this.select = select}
         >
           <option value="one">One</option>
