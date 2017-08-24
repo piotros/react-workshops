83f33d2671f2899ff9a30d4b6b77382635e42203
piotros <piotros@wp.pl>
2017-08-24 09:10:20 +0200

    Add new App component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.css';
 import {render} from 'react-dom';
-import Header from './components/Header';
+import App from './components/App';
 
-render(<Header />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
