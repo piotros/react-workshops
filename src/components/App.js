c896d7f4bcbaec7b184a0e0c071093fe18395eb9
piotros <piotros@wp.pl>
2017-08-24 10:01:12 +0200

    Add component as a prop extended

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,11 @@ export default function App(props) {
       <Header title="Dupa 1" {...props}/>
       <Header {...props} title="Dupa 2"/>
       <Header>Props children</Header>
-      <Header children={<Uppercase text="uppercase me"/>}/>
+      <Header children={<Uppercase text="uppercase me 1"/>}/>
+      <Header componentToUse={Uppercase}>uppercase me 2</Header>
+      <Header componentToUse={Uppercase} children="uppercase me 3"/>
+      <Header ComponentToUse={Uppercase}>uppercase me 4</Header>
+      <Header ComponentToUse={Uppercase} children="uppercase me 5"/>
     </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const Header = function (props) {
-  return <h1>{props.children}</h1>;
+const Header = function ({children, ComponentToUse, componentToUse}) {
+  if (componentToUse) {
+    return (
+      <h1>{React.createElement(componentToUse, {text: children})}</h1>
+    )
+  }
+
+  if (ComponentToUse) {
+    return (
+      <h1><ComponentToUse text={children}/></h1>
+    )
+  }
+
+  return <h1>{children}</h1>;
 };
 
 Header.defaultProps = {
